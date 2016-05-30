bb517c31e7015566e0063991391b1b0c30d90716
Jessa Wright <jessa.wright@gmail.com>
2016-05-29 22:53:31 -0500

    footer fix 6

--- javascripts/script.js
+++ javascripts/script.js
@@ -55,16 +55,7 @@ function showPusheenImages(imageList) {
         "bottom": "0",
         "padding-top": "60px",
     });
-
-    if ($("html").height() < $(".container-fluid").height()) {
-        $(".ground-background").css({
-            "border-bottom": "9999px solid yellow",
-        });
-        $("body").css({
-            "overflow": "hidden",
-        });
     }
-}
 /* Convert keywords to pngs and colors */
 function getPusheenImages(weatherCodeConditions) {
     $.getJSON("javascripts/weatherPNGJSON.js", function (pusheenImages) {
